09f96e5dbc9a28088cf5eed1ced3c5dc81dec84d
Técnico em Informática - 2018.2 <169121742018.2@senacrjedu.local>
2019-01-29 20:58:49 -0200

    versão inicial

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <h1>
+                <a href="<?php echo get_attachment_link(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h1>
+            <p><?php the_content(); ?></p>
+
+            <?php endwhile; else : ?>
+                <p><?php _e( 'Nenhum post encontrado!' ); ?></p>
+            <?php endif; ?>
+        
+        </div>
+        <div class="col-3">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
